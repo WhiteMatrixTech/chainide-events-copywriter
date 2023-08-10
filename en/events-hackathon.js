418190eb80fc08aa44e2d42d2cecbff25a5b285e
feibowei <1906524955@qq.com>
2023-08-10 18:46:13 +0800

    feat: add event hackathon

---
+++ en/events-hackathon.js
@@ -0,0 +1,16 @@
+export const events_hackathon = {
+  events_hackathon_title: 'Hackathon',
+  events_hackathon_content: 'The hackathons gather the most active developers in the community to conduct fun hacks in a short time, aiming to expand the blockchain ecosystem.',
+  events_hackathon_list: [
+    {
+      image: 'https://media.chainide.com/events/hackathon/EthHackthon.png',
+      title: 'ETH Hackathon',
+      date: '2021.05.14 - 2021.05.16',
+      internalPath: `${window.location.origin}/events/eth-hackathon`,
+      content: 'The theme of the ETH hackathon was "open, free and incremental". Hackers needed to build a product prototype based on Ethereum within 48 hours. The event aimed to prosper the Ethereum developer ecosystem.',
+      weChat1: 'https://mp.weixin.qq.com/s/Gi_aalqRBf7DVnjXsx4HPQ',
+      weChat2: 'https://mp.weixin.qq.com/s/1RJagdcCV1lnvSuu2rPYZA',
+      weChat3: 'https://mp.weixin.qq.com/s/6ymSKVY-LLwwqMzT3qx4WA',
+    },
+  ],
+};
---
+++ zh/events-hacthon.js
@@ -0,0 +1,16 @@
+export const events_hackathon = {
+  events_hackathon_title: '黑客松',
+  events_hackathon_content: '黑客松集结社区中最活跃的技术开发者，以极短的时间进行畅快Hack，旨在繁荣区块链社区生态。',
+  events_hackathon_list: [
+    {
+      image: 'https://media.chainide.com/events/hackathon/EthHackthon.png',
+      title: 'ETH Hackathon',
+      date: '2021.05.14 - 2021.05.16',
+      content: 'ETH黑客松以“开放、自由、增量”为主题，黑客们需在48小时内构建出基于以太坊的产品原型，活动旨在繁荣以太坊开发者生态。',
+      internalPath: `${window.location.origin}/events/eth-hackathon`,
+      weChat1: 'https://mp.weixin.qq.com/s/Gi_aalqRBf7DVnjXsx4HPQ',
+      weChat2: 'https://mp.weixin.qq.com/s/1RJagdcCV1lnvSuu2rPYZA',
+      weChat3: 'https://mp.weixin.qq.com/s/6ymSKVY-LLwwqMzT3qx4WA',
+    },
+  ],
+};

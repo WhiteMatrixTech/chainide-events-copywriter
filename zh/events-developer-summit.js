ed4505d0b5aa7d61bf9d9bed5773b4062cf600bb
chenjiali-april <132745538+chenjiali-april@users.noreply.github.com>
2023-08-25 09:12:38 +0800

    Update events-developer-summit.js

--- zh/events-developer-summit.js
+++ zh/events-developer-summit.js
@@ -2,7 +2,7 @@ export const events_developer_summit = {
   events_developer_summit_title: '开发者峰会',
   events_developer_summit_content: '这些峰会和会议将我们的团队成员与来自不同领域和领域的开发者、专业人士和行业领袖汇聚在一起。他们与全球科技行业的杰出思想领袖一起积极参与讨论，为各自专业领域传授独特的见解和经验。',
 
-  upcoming_events_image: 'https://media.chainide.com/events/previousActivities/eth-riyadh-2023.png',
+  upcoming_events_image: 'https://media.chainide.com/events/riyadh-summit.png',
   upcoming_events_title: 'ETH Riyadh 2023',
   upcoming_events_content: 'ETH Riyadh 2023是一个专注于以太坊的重要Web3峰会，旨在汇集来自沙特阿拉伯王国和其他中东地区的开发者和构建者。EthRiyadh 强调以太坊生态系统，旨在提供行业专家的见解，并参与有关区块链未来的讨论。',
   upcoming_events_luma_link: 'https://ethriyadh.com/',
